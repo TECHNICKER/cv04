1cccfdf063dfece2e0bc550d59cf82b29cc6d100
Student <Student@PC-SE5113-14>
2023-10-12 08:18:59 +0200

    cv04 DONE

--- cv04/cv04.cpp
+++ cv04/cv04.cpp
@@ -1,12 +1,51 @@
-﻿// cv04.cpp : Defines the entry point for the application.
-//
+﻿#include "cv04.h"
 
-#include "cv04.h"
-
-using namespace std;
+/*
+int isLeapYear(int year)
+{
+	if ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0)
+	{
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	int array[6] = {1000, 2000, 2002, 2012, 2022, 2200};
+
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%d %d\n", array[i], isLeapYear(array[i]));
+	}
+
+	return 0;
+}*/
+
+
+double factorial(double operand)
+{
+	double vysledek = operand;
+
+	for (int i = 1; i <= operand; i++)
+	{
+		vysledek = vysledek * i;
+	}
+
+	return vysledek;
+}
+
+int main()
+{
+	double operand = 0;
+
+	while (1)
+	{
+		scanf("%lf", &operand);
+		printf("%lf\n", factorial(operand));
+	}
+
 	return 0;
 }
